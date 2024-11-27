51396d82c113954f2510026c9c86af868fd4005f
Marina De Castro <132936941+cyn1calmars@users.noreply.github.com>
2024-11-27 12:32:36 -0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="EN">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Index.css">
+<title>Home Page</title>
+</head>
+<body>
+  <div>
+    <h1>Welcome to Mars Website</h1>
+    
+    <h2>Project List</h2>
+<a href="Baker.php">1. Baker Application Form</a>
+<br>
+<a href="Countdown.php">2. Countdown Cookie Sweepstakes </a>
+</div>
+</body>
+</html>
